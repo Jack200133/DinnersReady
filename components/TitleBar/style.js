19c4d5a42973f393bf1a5b39cdbc8d610df0c85d
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-02-24 15:58:04 -0600

    IconBar and TitleBar

---
+++ components/TitleBar/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        paddingTop:0,
+        height: 100,
+        paddingHorizontal: 5,
+        alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#f36c6c' 
+    }
+});
+
+export default styles;
