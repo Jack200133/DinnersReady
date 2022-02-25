5379e6834c48c826a966a8bf5a3306f801821a40
Daniel14gonc <71653466+Daniel14gonc@users.noreply.github.com>
2022-02-24 18:06:23 -0600

    ScrollView Receta

--- views/recipe.js
+++ views/recipe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, ScrollView } from 'react-native';
 
 import StyledButton from '../components/StyledButton';
 import NavBar from '../components/NavBar';
@@ -8,8 +8,15 @@ function RecipeScreen(props) {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <View style={styles.container}/>
+      <ImageBackground source={require('../assets/images/fondo.png')}
+            resizeMode="cover"
+            style={styles.back}>
+        <View style={styles.padContainer}>
+          <ScrollView style={styles.scrollCont}>
+            <Text style={styles.text}>Ingresa tu nueva receta</Text>
+          </ScrollView>
+        </View>
+      </ImageBackground>
       <NavBar/>
     </View>
   );
@@ -18,6 +25,32 @@ function RecipeScreen(props) {
 const styles = StyleSheet.create({
   container: {
     flex:1
+  },
+  padContainer: {
+    flex:1,
+    paddingHorizontal: 10,
+    paddingTop: 180,
+    paddingBottom: 20
+  },
+  back:{
+    width: '100%',
+    height: '100%',
+    flex: 1, 
+    justifyContent: "center",
+    opacity: 0.3
+  },
+  scrollCont: {
+    flex:1,
+    backgroundColor: 'white',
+    borderRadius: 30,
+    borderColor: 'black',
+    borderWidth: 2,
+    padding: 30
+  },
+  text: {
+    fontFamily: 'PFD',
+    fontSize: 30,
+    color: 'black'
   }
 });
 
