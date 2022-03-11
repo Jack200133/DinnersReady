320496bf19ba775db4944498d352e6d3ad0bce95
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-03-10 18:19:52 -0600

    App.js

--- App.js
+++ App.js
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import RecipeScreen from './views/recipe';
-import AddIngredientsScreen from './views/addIngredients'
 
 export default function App() {
   return (
-    <AddIngredientsScreen/>
+    <RecipeScreen/>
   );
 }
 
