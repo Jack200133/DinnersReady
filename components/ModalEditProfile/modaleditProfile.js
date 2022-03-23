d236cddaaa5243e081efe22d06a96d9b57d2c150
franco611 <62269078+franco611@users.noreply.github.com>
2022-03-22 18:25:07 -0600

    Modificacion de modal
    Se agregaron diferentes elementos enm el modal

--- components/ModalEditProfile/modaleditProfile.js
+++ components/ModalEditProfile/modaleditProfile.js
@@ -1,26 +1,76 @@
 import React from "react"
-import { StyleSheet, Text, View, ImageBackground, Image, ScrollView, Pressable } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, ScrollView, Pressable, SafeAreaView,TextInput } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker';
 
 import styles from './style.js';
 
 
 const ModaleditProfile = ({descripcion, imagen, toggleModal}) =>{
 
+  const [image, setImage] = React.useState(null);
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+  const [desc, onChangeDesc] = React.useState(null);
+
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
 
   return(
-    <View style = {styles.container}>
-      <View style = {styles.buttons}>
-        <Pressable onPress={toggleModal}>
-          <Text>Cancelar</Text>
-        </Pressable>
-        <Pressable >
-          <Text>Aceptar</Text>
-        </Pressable>
+    <SafeAreaView style = {{flex:1, }}>
+      <View style = {styles.container}>
+        <View style = {styles.buttons}>
+          <View>
+            <Pressable onPress={toggleModal}>
+              <Text style = {{fontSize: 18, color:'#EEE9E9'}}>Cancelar</Text>
+            </Pressable>
+          </View>
+          <View>
+            <Pressable onPress={toggleModal}>
+              <Text style = {{fontSize: 18, color:'#EEE9E9'}}>Aceptar</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style = {styles.PickerContainer}>
+          <View style={styles.imagePicker}>
+            <Text style={styles.titular}>Presione imagen para cambiar</Text>
+            <Pressable style={styles.button} onPress={pickImage}>
+            {
+                image ? <Image resizeMode='cover' style={{width: '100%', height: '100%', borderRadius: 30}} 
+                source = {{uri: image}}/> :
+                <Image resizeMode='contain' style={styles.addImage} 
+                source = {imagen}/>
+            }
+            </Pressable>
+          </View>
+          <View style = {{marginVertical: 15, flex:1}}>
+            <Text style = {{marginTop: 45, marginLeft: 20, fontSize: 15}} >Ingrese nueva descripcion</Text>
+            <TextInput
+                style={styles.inputDesc}
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder = {descripcion}
+                multiline = {true}
+                keyboardType="default"
+              />
+          </View>
+        </View>
       </View>
-      <View>
-
-      </View>
-    </View>
+    </SafeAreaView>
   )
 }
 export default ModaleditProfile;
--- components/ModalEditProfile/style.js
+++ components/ModalEditProfile/style.js
@@ -4,14 +4,67 @@ import { StyleSheet} from "react-native-web"
 
 const styles = StyleSheet.create({
   buttons:{
+    width: '100%',
+    height: 50,
     display:'flex',
-    justifyContent:'space-around'
+    flexDirection:'row',
+    justifyContent:'space-between',
+    backgroundColor: '#f36c6c',
+    marginTop:0,
+    paddingHorizontal: 30,
+    alignItems:'center',
   },
   container:{
     display:'flex',
     flexDirection:'column',
-    justifyContent:'center',
-    flex:1,
+    alignItems:'center',
+    flex:3,
+    backgroundColor: '#EEE9E9'
+  },
+  imagePicker: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  PickerContainer: {
+    width: '100%',
+    height: 1000,
+    display: 'flex',
+  },
+  addImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 100
+  },
+  inputDesc:{
+    borderWidth: 1,
+    margin: 8,
+    borderColor: 'black',
+    borderRadius: 30,
+    fontSize: 18,
+    height: 170,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    backgroundColor: '#EEE9E9'
+  },
+  titular:{
+    width: 300,
+    marginVertical: 15,
+    fontSize: 15,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    marginLeft: '50%'
   }
 
 });
