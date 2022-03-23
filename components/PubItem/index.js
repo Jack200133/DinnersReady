865dab0394327eaa6afd7336049c4d69d6ac52a1
Jack200133 <car20593@uvg.edu.gt>
2022-03-22 19:34:51 -0600

    Cambios a vista de Recetas
    Se agrego la dificultad, rating y el boton de guardado de las recetas
    
    Co-Authored-By: Aristondo01 <70415140+Aristondo01@users.noreply.github.com>
    Co-Authored-By: JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>

--- components/PubItem/index.js
+++ components/PubItem/index.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import {View,ImageBackground,Image,Text} from 'react-native';
-
 import styles from './style';
+
+const star = require('../../assets/images/star.png')
 
 const PubItem = (props) => {
     return (
         
         <View style = {styles.container}>
-            <Image 
-            source={props.image} 
-            style={styles.back}>
-            </Image>
-            <Text style={styles.Title}>{props.NameRecipe}</Text>
+            <View style={styles.vistas}>
+                
+                    <View style = {{...styles.TextBack,backgroundColor: props.color}}>
+                        <Text style={styles.Dificult} >{props.dificultad}</Text>
+                    </View>
+                    <View style = {styles.SavedContainer}>
+                        <Image source={props.saved} 
+                        resizeMode="contain" style={styles.Image}>  
+                            
+                        </Image>
+                    </View>
+                    
+                         
+            </View>
+            <Image source={props.image} resizeMode="contain" style={styles.background}>
+                </Image>   
+            <View style = {styles.TitleContainer}>
+                <Text style={styles.Title}>{props.NameRecipe}</Text>
+                <View style = {styles.Stars}>
+                    
+                    <View style = {styles.SavedContainer}>
+                        <Image source={star} 
+                        resizeMode="contain" style={styles.Estrella}>  
+                            
+                        </Image>
+                    </View>
+                    <Text style={styles.TextStar}> {props.stars}</Text>
+                </View>
+            </View>
             <Text style={styles.Descripcion}>{props.desc}</Text>
-            
+            <Text style = {styles.Hashtag}>{props.hash} </Text>
         </View>
     );
 };
