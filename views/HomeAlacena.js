ddde62de450017e5b0f209cc4987588df4fd2eda
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-06-01 15:36:21 -0600

    Revert "Merge branch 'Book-icon' into develop"
    This reverts commit 9e06062dc01cf9fb229053dbb90eece13550ac3b, reversing
    changes made to ac4668cf4a5a10f3e724a66710b1fc4b2a318da1.

--- views/HomeAlacena.js
+++ views/HomeAlacena.js
@@ -17,7 +17,7 @@ function HomeAlacena(props) {
   const Market_Recipe = require('../assets/images/cart.png');
   const Hamburguesa = require('../assets/images/hamburguesa_temporal.jpg');
   const Saved = require('../assets/images/bookmark.png')
-  const Savednt = require('../assets/images/bookmarkn.png')
+  const Savednt = require('../assets/images/bookmarkN.png')
 
   const [recetas, setRecetas] = React.useState([])
   const [savedrecipe, setSaved] = React.useState([])
