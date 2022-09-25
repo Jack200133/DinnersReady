97e2923dc3ed357cfad402ccfba7f822c324e23e
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-09-24 18:55:13 -0600

    test terminado de navsearch

--- components/NavSearch/index.js
+++ components/NavSearch/index.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import { SafeAreaView } from "react-native";
 import { SearchBar } from "react-native-elements";
 import Styles from './style';
-
 const SearchBart = () => {
     const [searchQuery, setSearchQuery] = React.useState('');
-
+    //const [value, setValue] = React.useState('');
     const onChangeSearch = query => setSearchQuery(query);
 
   return (
     <SafeAreaView style = {Styles.container}>
         <SearchBar
+            testID="search_bar"
             placeholder="Buscar..."
             onChangeText={onChangeSearch}
             value={searchQuery}
@@ -18,11 +18,12 @@ const SearchBart = () => {
             rightIconContainerStyle = {Styles.icon}
             onKeyPress={(e) => {
               if (e.key=='Enter'){
-                console.log(e.target.value)}//En vez del console iria un query
+                //setValue(e.target.value)
+                //En vez del console iria un query
                 // Deberiamos mandar una funcion en props para que busque dependiendo de donde este.
                 // Similar al press y long press de alacena.
               } 
-            }
+            }}
             round = {true}
             inputStyle={Styles.input}
             searchIconColor = '#05182A'
