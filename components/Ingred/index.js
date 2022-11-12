82b0f49927bc38344570206dd8c833d510977086
franco611 <62269078+franco611@users.noreply.github.com>
2022-11-12 17:31:11 -0600

    Prueba Terminada

--- components/Ingred/index.js
+++ components/Ingred/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {View,Text, Pressable} from 'react-native';
+import usePress from "use-double-press";
 import styles from './style';
 
 
 const Ingred = ({text, index, removeIngred, AddIngrediente,IndexC}) => {
+
+
+    const pressHandler = usePress({
+        onPress: () => {AddIngrediente(text,IndexC,index)},
+        onDoublePress: () => {removeIngred(text)},
+        delay: 200,
+    });
     
     return (
-        <Pressable nativeID={text} style = {styles.container} onPress={() => AddIngrediente(text,IndexC,index)} onLongPress={() => removeIngred(text)}>
+        <Pressable nativeID={text} style = {styles.container} onPress={pressHandler}>
             <Text  style = {{fontFamily:'Mulish_Regular'}}>{text}</Text>
         </Pressable>
     );
