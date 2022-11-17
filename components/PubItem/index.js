331698667bc15d92cc209ced3ebd9b5d1fe21b8a
Daniel <gon20293@uvg.edu.gt>
2022-11-16 19:15:05 -0600

    id solved

--- components/PubItem/index.js
+++ components/PubItem/index.js
@@ -81,7 +81,7 @@ const PubItem = ({id,image,color,dificultad,saved,cH,NameRecipe,stars,hash,desc,
     //console.log(props.navigation.navigate)
     //const navigate = useNavigate()
     return (
-        <Pressable nativeID={id} testID={id} style = {styles.container} onPress={()=> {save(id, navigation)}}>
+        <Pressable nativeID={id} testID='Guardado' style = {styles.container} onPress={()=> {save(id, navigation)}}>
             <View>
                 <View style={styles.vistas}>
                         {
